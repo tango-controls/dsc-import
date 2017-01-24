d6f2d2c439aa7d930ed2b23f15c8405220d4c9ce
Piotr Goryl <piotr.goryl@3-controls.com>
2017-01-24 16:31:28 +0100

    DSC-83, html pareser implemented

--- xmi_from_html.py
+++ xmi_from_html.py
@@ -19,6 +19,88 @@
 
 from lxml import etree
 from lxml.html import parse
+import re
+
+DS_COMMAND_DATATYPES = {
+    'BooleanType': 'DevBoolean',
+    'FloatType': 'DevFloat',
+    'DoubleType': 'DevDouble',
+    'IntType': 'DevLong',
+    'LongType': 'DevLong64',
+    'ShortType': 'DevShort',
+    'StringType': 'DevString',
+    'UIntType': 'DevULong',
+    'ULongType': 'DevULong64',
+    'UShortType': 'DevUShort',
+    'UCharType': 'DevUChar',
+    'CharType': 'DevChar',
+    'CharArrayType': 'DevVarCharArray',
+    'DoubleArrayType': 'DevVarDoubleArray',
+    'DoubleStringArrayType': 'DevVarDoubleStringArray',
+    'FloatArrayType': 'DevVarFloatArray',
+    'LongArrayType': 'DevVarLong64Array',
+    'IntArrayType': 'DevVarLongArray',
+    'LongStringArrayType': 'DevVarLongStringArray',
+    'ShortArrayType': 'DevVarShortArray',
+    'StringArrayType': 'DevVarStringArray',
+    'ULongArrayType': 'DevVarULong64Array',
+    'UIntArrayType': 'DevVarULongArray',
+    'UShortArrayType': 'DevVarUShortArray',
+    'VoidType': 'DevVoid',
+    'ConstStringType': 'ConstDevString',
+    'StateType': 'State'
+}
+DS_COMMAND_DATATYPES_REVERS = {v: k for k, v in DS_COMMAND_DATATYPES.items() }
+DS_COMMAND_DATATYPES_REVERS['DevState']='StateType'
+
+
+
+
+DS_ATTRIBUTE_DATATYPES = {
+    'BooleanType': 'DevBoolean',
+    'FloatType': 'DevFloat',
+    'DoubleType': 'DevDouble',
+    'IntType': 'DevLong',
+    'LongType': 'DevLong64',
+    'ShortType': 'DevShort',
+    'StringType': 'DevString',
+    'UIntType': 'DevULong',
+    'ULongType': 'DevULong64',
+    'UShortType': 'DevUShort',
+    'UCharType': 'DevUChar',
+    'CharType': 'DevChar',
+    'EncodedType': 'DevEncoded',
+    'StateType': 'DevState'
+}
+
+DS_ATTRIBUTE_DATATYPES_REVERS = {v: k for k, v in DS_ATTRIBUTE_DATATYPES.items() }
+
+DS_PROPERTIES_DATATYPES = {
+    'BooleanType': 'DevBoolean',
+    'FloatType': 'DevFloat',
+    'DoubleType': 'DevDouble',
+    'IntType': 'DevLong',
+    'LongType': 'DevLong64',
+    'ShortType': 'DevShort',
+    'StringType': 'DevString',
+    'UIntType': 'DevULong',
+    'ULongType': 'DevULong64',
+    'UShortType': 'DevUShort',
+    'UCharType': 'DevUChar',
+    'CharType': 'DevChar',
+    'BooleanVectorType': 'Array of DevBoolean',
+    'FloatVectorType': 'Array of DevFloat',
+    'DoubleVectorType': 'Array of DevDouble',
+    'IntVectorType': 'Array of DevLong',
+    'LongVectorType': 'Array of DevLong64',
+    'ShortVectorType': 'Array of DevShort',
+    'StringVectorType': 'Array of DevString',
+    'UIntVectorType': 'Array of DevULong',
+    'ULongVectorType': 'Array of DevULong64',
+    'UShortVectorType': 'Array of DevUShort',
+    'UCharVectorType': 'Array of DevUChar',
+    'CharVectorType': 'Array of DevChar',
+}
 
 
 
@@ -43,44 +125,188 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:pogoDsl="http://www.
     description_xml = etree.SubElement(classes_xml,'description')
     identification_xml = etree.SubElement(description_xml,'identification')
 
+    # search for name, keywords and so
     for el in description_parser.iter('td'):
         # name
         if el.text is not None and el.text.strip().lower() == 'class':
             classes_xml.set('name', str(el.getnext().text).split(':')[1].strip())
 
+
         # family
-        if el.text is not None and el.text.strip().lower == 'family':
+        if el.text is not None and el.text.strip().lower() == 'family':
             identification_xml.set('classFamily', str(el.getnext().text).split(':')[1].strip())
 
         # key words
-        if el.text is not None and el.text.strip().lower == 'key word(s)':
+        if el.text is not None and el.text.strip().lower() == 'key word(s)':
             for kw in str(el.getnext().text).split(':'):
                 kw_el = etree.SubElement(identification_xml,'keyWords')
                 kw_el.text = str(kw).strip()
 
         # author
-        if el.text is not None and el.text.strip().lower == 'author':
+        if el.text is not None and el.text.strip().lower() == 'author':
             identification_xml.set('contact', str(el.getnext().text).split(':')[1].strip())
 
         # language
-        if el.text is not None and el.text.strip().lower == 'language':
+        if el.text is not None and el.text.strip().lower() == 'language':
             description_xml.set('language', str(el.getnext().text).split(':')[1].strip())
 
         # platform
-        if el.text is not None and el.text.strip().lower == 'platform':
+        if el.text is not None and el.text.strip().lower() == 'platform':
             identification_xml.set('platform', str(el.getnext().text).split(':')[1].strip())
 
         # bus
-        if el.text is not None and el.text.strip().lower == 'bus':
+        if el.text is not None and el.text.strip().lower() == 'bus':
             identification_xml.set('bus', str(el.getnext().text).split(':')[1].strip())
 
         # manufacturer
-        if el.text is not None and el.text.strip().lower == 'manufacturer':
+        if el.text is not None and el.text.strip().lower() == 'manufacturer':
             identification_xml.set('manufacturer', str(el.getnext().text).split(':')[1].strip())
 
         # Manufacturer ref
-        if el.text is not None and el.text.strip().lower == 'manufacturer ref.':
+        if el.text is not None and el.text.strip().lower() == 'manufacturer ref.':
             identification_xml.set('reference', str(el.getnext().text).split(':')[1].strip())
+
+    description_is_comming = False
+    # serch for descritpion text itself
+    for el in description_parser.iter():
+        if el.text is not None and el.text.strip() == '%s Class Description :' % classes_xml.get('name'):
+            description_is_comming = True
+        if description_is_comming and el.tag=='ul' and el.text is not None:
+            description_xml.set('description', el.text.strip())
+
+    # attributes
+    headers = []
+
+    if attributes_url!='':
+        attributes_parser = parse(attributes_url)
+        for tr in attributes_parser.iter('tr'):
+            if tr.getprevious() is None:
+                headers = []
+            elif tr.getprevious() is not None and len(headers)==0:
+                for td in tr.iter('td'):
+                    head = td.text_content().strip().lower()
+                    headers.append(head)
+            elif tr.getprevious() is not None and len(headers)>0:
+                i = 0
+                attribute_xml = etree.SubElement(classes_xml,'attributes')
+                for td in tr.iter('td'):
+
+                    if headers[i] == 'name':
+                        attribute_xml.set('name', td.text_content().strip())
+
+                    if headers[i] == 'description':
+                        attribute_xml.set('description', td.text_content().strip())
+
+                    if headers[i] == 'attr. type':
+                        attribute_xml.set('attType', td.text_content().strip())
+
+                    if headers[i] == 'data type':
+                        type_parsed = re.match('.*(DEV)_(U)*(.*)', td.text_content().strip())
+                        if type_parsed is not None:
+                            tango_type = ''
+                            for g in [1,2,3]:
+                                if type_parsed.group(g) is not None:
+                                    tango_type += type_parsed.group(g).capitalize()
+                            # print tango_type
+                            etree.SubElement(attribute_xml, 'dataType',
+                                             attrib={etree.QName('http://www.omg.org/XMI','type'): 'pogoDsl:' +
+                                                            DS_ATTRIBUTE_DATATYPES_REVERS.get(tango_type,'Unknown')})
+
+                    i += 1
+
+    # commands
+    headers = []
+
+    if commands_url != '':
+        commands_parser = parse(commands_url)
+        for tr in commands_parser.iter('tr'):
+            if tr.getprevious() is None:
+                headers = []
+            elif tr.getprevious() is not None and len(headers) == 0:
+                for td in tr.iter('td'):
+                    head = td.text_content().strip().lower()
+                    headers.append(head)
+
+            elif tr.getprevious() is not None and len(headers) > 0:
+                i = 0
+                command_xml = etree.SubElement(classes_xml, 'commands')
+                for td in tr.iter('td'):
+
+                    if headers[i] == 'name':
+                        command_xml.set('name', td.text_content().strip())
+                        # print 'Command: %s' % command_xml.get('name')
+
+                    if headers[i] == 'description':
+                        command_xml.set('description', td.text_content().strip())
+
+                    if headers[i] == 'input type':
+                        # print 'input:'
+                        type_parsed = re.match('.*(CONST)*_*(DEV)(VAR)*_(U)*((.*)(STRING)(ARRAY)*|(.*)(ARRAY)|(.*(?!ARRAY)))',
+                                               td.text_content().strip())
+
+                        if type_parsed is not None:
+                            tango_type = ''
+                            for g in [1,2,3,4,6,7,8,9,10,11]:
+                                if type_parsed.group(g) is not None:
+                                    tango_type += type_parsed.group(g).capitalize()
+
+                            # print '%s: %s' % (tango_type, DS_COMMAND_DATATYPES_REVERS.get(tango_type, 'Unknown'))
+                            argin_xml = etree.SubElement(command_xml,'argin')
+                            etree.SubElement(argin_xml, 'type',
+                                             attrib={etree.QName('http://www.omg.org/XMI', 'type'): 'pogoDsl:' +
+                                                                DS_COMMAND_DATATYPES_REVERS.get(tango_type, 'Unknown')})
+                    if headers[i] == 'output type':
+                        # print 'output:'
+                        type_parsed = re.match('.*(CONST)*_*(DEV)(VAR)*_(U)*((.*)(STRING)(ARRAY)*|(.*)(ARRAY)|(.*(?!ARRAY)))',
+                                               td.text_content().strip())
+
+                        if type_parsed is not None:
+                            tango_type = ''
+                            for g in [1,2,3,4,6,7,8,9,10,11]:
+                                if type_parsed.group(g) is not None:
+                                    tango_type += type_parsed.group(g).capitalize()
+
+                            # print '%s: %s' % (tango_type, DS_COMMAND_DATATYPES_REVERS.get(tango_type, 'Unknown'))
+                            argout_xml = etree.SubElement(command_xml,'argout')
+                            etree.SubElement(argout_xml, 'type',
+                                             attrib={etree.QName('http://www.omg.org/XMI', 'type'): 'pogoDsl:' +
+                                                                DS_COMMAND_DATATYPES_REVERS.get(tango_type, 'Unknown')})
+
+                    i += 1
+
+    # properties
+    headers = []
+
+    if properties_url != '':
+        properties_parser = parse(properties_url)
+        for tr in properties_parser.iter('tr'):
+
+            if tr.getprevious() is None:
+                headers = []
+
+            elif tr.getprevious() is not None and len(headers) == 0:
+                for td in tr.iter('td'):
+                    head = td.text_content().strip().lower()
+                    headers.append(head)
+
+            elif tr.getprevious() is not None and len(headers) > 0:
+                i = 0
+                property_xml = etree.SubElement(classes_xml,'deviceProperties')
+                for td in tr.iter('td'):
+
+                    if headers[i] == 'name':
+                        property_xml.set('name', td.text_content().strip())
+
+                    if headers[i] == 'descritpion':
+                        property_xml.set('description', td.text_content().strip())
+
+                    if headers[i] == 'type':
+                        property_type_xml = etree.SubElement(property_xml,'type')
+                        property_type_xml.set(etree.QName('http://www.omg.org/XMI', 'type'),
+                                         'pogoDsl:'+ td.text_content().strip().capitalize() + 'Type')
+
+                    i += 1
+
 
     return etree.tostring(xmi_xml)
 
@@ -95,14 +321,17 @@ def test_xmi_from_html():
         print "-------------------------------"
         print "Testing for URL: " + url
         print "-------------------------------"
-        xmi = get_xmi_from_html(description_url=url+'/ClassDescription.html')
+        xmi = get_xmi_from_html(description_url=url+'/ClassDescription.html',
+                                attributes_url=url+'/Attributes.html',
+                                commands_url=url + '/Commands.html',
+                                properties_url= url + '/Properties.html')
 
         print "\nXMI content:"
         print xmi;
         print "--------------------------------"
         print
 
-test_xmi_from_html()
+# test_xmi_from_html()
 
 
 
