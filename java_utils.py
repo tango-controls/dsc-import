300ecd4fa339cdf2c10fec921d4ae90a0bf4b21e
Piotr Goryl <piotr.goryl@3-controls.com>
2018-01-17 11:09:29 +0100

    Java to XMI conversion implemented

--- java_utils.py
+++ java_utils.py
@@ -24,160 +24,13 @@ from lxml import etree
 from xmi_utils import *
 
 
-class DefaultCopyDict(dict):
-    """ Just to provide a dictionary with default value for a missing keys """
-
-    def __missing__(self, key):
-        return key
-
-class DispLevel:
-
-    EXPERT = 'EXPERT'
-    OPERATOR = 'OPERATOR'
-
-class PyTangoClass:
-    """ This is a mock class to allow eval on member lists"""
-
-    def __init__(self):
-
-        # build conversion table
-        conversion_table = DefaultCopyDict()
-        conversion_table.update(DS_ATTRIBUTE_DATATYPES_REVERS)
-        conversion_table.update(DS_COMMAND_DATATYPES_REVERS)
-        conversion_table.update({
-            "READ": "READ",
-            "READ_WRITE": "READ_WRITE",
-            "WRITE": "WRITE",
-            "READ_WITH_WRITE":  "READ_WITH_WRITE",
-            "IMAGE": "Image",
-            "SPECTRUM": "Spectrum",
-            "SCALAR": "Scalar"
-        })
-        conversion_table.update({
-            "DispLevel": DispLevel(),
-        })
-
-        # do a trick to populate PyTango class with conversion members
-        # self.__class__.__dict__ = conversion_table
-        self.__dict__ = conversion_table
-
-    def __missing__(self, key):
-        return key
-
-# PyTango mock
-PyTango = PyTangoClass()
-
-
-def get_line_indentation(line):
-    """ Returns indentation of the line"""
-    assert isinstance(line, str)
-    return len(line)-len(line.lstrip())
-
-
-def get_class_content(source_lines, class_name):
-    """ Provides only lines defining class content
-    :return: lines belonging to class
+def get_xmi_from_java(name, family, java_file_url, element=None):
     """
-    class_started = False
-    class_indentation = 0
-    class_lines = []
-
-    # regular expression to detect class definition
-    match_string = r'(\s*class\s+)(?P<class_name>'+class_name+r')(.+)'
-    expr = re.compile(match_string)
-
-    for line in source_lines:
-
-        assert isinstance(line, str)
-
-        if not class_started:
-            # detect beginning of class
-            match_result = expr.match(line)
-
-            if match_result is None:
-                continue
-
-            # if match_result.group('class_name').strip() == class_name:
-            else:
-                class_started = True
-                class_indentation = get_line_indentation(line)
-                class_lines.append(line)
-
-        elif get_line_indentation(line) > class_indentation \
-                or line.lstrip().startswith('#') \
-                or len(line.strip()) == 0:
-
-            # add all lines indented properly and comments
-            class_lines.append(line)
-
-        else:
-            # this is a case when the line is first one not belonging to the class
-            break
-
-    return class_lines
-
-
-def get_class_dict_member(source_lines, member_name):
-    """
-    Returns (eval) dictionary class member value. Useful for finding lists of attributes, commands and properties
-    in device class definition
-    :param source_lines: source content
-    :param member_name: name of a member to read from class definition
-    :return:
-    """
-
-    match_string = r'\s*'+member_name+'\s*=\s*(?P<content>.*)'
-    expr = re.compile(match_string)
-
-    member_found = False
-    brackets_count = 0
-    to_eval = ''
-
-    for line in source_lines:
-
-        assert isinstance(line, str)
-
-        if not member_found:
-            # find beginning of member
-            match_result = expr.match(line)
-
-            if match_result is not None:
-                # fund match, limit line to the content
-                member_found = True
-                line = match_result.group('content').strip()
-            else:
-                continue
-
-        if member_found and not line.lstrip().startswith('#'):
-            # updated bracket bilans
-            brackets_count += line.count('{') - line.count('}')
-            # add line to be evaluated
-            to_eval += '\n' + line
-
-            if brackets_count == 0:
-                # the closing bracket found
-                break
-
-    # return evaluation of the member
-    return eval(to_eval)
-
-
-def get_class_member_comment(source_lines, member_name):
-    """
-    Get comment located just before a class member or its doc string.
-    :param source_lines: source content
-    :param member_name: name of a member
-    :return: (comment, docstring)
-    """
-    pass
-
-
-def get_xmi_from_java(name, family, java_file_url, element = None):
-    """
-
+    Parse a java file and generates xmi based on its contents
     :param name: name of the device server
     :param family: family name
-    :param python_file_url: URL of the python file
+    :param java_file_url: URL of the java file
+    :param element: SVN element related to this file (to get some additional information)
     :return: content of xmi file derived from the python source
     """
 
@@ -203,15 +56,11 @@ def get_xmi_from_java(name, family, java_file_url, element = None):
     assert isinstance(ds_source, str)
     source_lines = ds_source.splitlines()
 
-
     author = ''
-
-
     copyleft = ''
-
+    comment_buffer = ''
 
     # generate xmi
-    comment_buffer = ''
     classes_xml = None
     description_xml = None
     identification_xml = None
@@ -255,7 +104,7 @@ def get_xmi_from_java(name, family, java_file_url, element = None):
             comment_buffer = ''
 
             # get class name
-            index +=1
+            index += 1
             line = source_lines[index]
             match_result = re.match(r".*(?=class)class\s+(?P<class_name>[^\s({:]+)", line)
 
@@ -360,6 +209,7 @@ def get_xmi_from_java(name, family, java_file_url, element = None):
 
             # get description of argin and argout
             argin_description = ''
+            argin_type = 'VoidType'
             argout_description = ''
 
             match_result = re.match(r'inTypeDesc\s*=\s*"(?P<desc>[^"]+)"', line)
@@ -377,7 +227,7 @@ def get_xmi_from_java(name, family, java_file_url, element = None):
             # command name and type
             line = ' ' + source_lines[index].strip()
             match_result = re.match(
-                r'.*(?=\s[a-zA-Z]+[[]]*\s+[a-zA-Z0-9]+\s*[=;])'
+                r'.*(?=\s[a-zA-Z]+[[]]*\s+[a-zA-Z0-9]+\s*[(])'
                 r'\s(?P<type>[a-zA-Z]+[[]]*)\s+(?P<name>[a-zA-Z0-9]+)\s*[(]',
                 ' ' + line
             )
@@ -419,76 +269,110 @@ def get_xmi_from_java(name, family, java_file_url, element = None):
                 'pogoDsl:' + argout_type
             )
 
+        # capture pipes
+        if classes_xml is not None and line.strip().startswith('@Pipe'):
+
+            pipe_description = comment_buffer
+            comment_buffer = ''
+
+            # walk through other decorations
+            while source_lines[index].strip().startswith('@'):
+                index += 1
+
+            # pipe name and type
+            line = ' ' + source_lines[index].strip()
+            match_result = re.match(
+                r'.*(?=\s[a-zA-Z]+[[]]*\s+[a-zA-Z0-9]+\s*[=;])'
+                r'\s(?P<type>[a-zA-Z]+[[]]*)\s+(?P<name>[a-zA-Z0-9]+)\s*[=;]',
+                ' ' + line
+            )
+            if match_result is None:
+                continue
+            else:
+                pipe_name = match_result.group('name')                
+
+            # create xml entries
+            pipe_xml = etree.SubElement(classes_xml, 'pipes')
+
+            # populate fields
+            pipe_xml.set('name', pipe_name)
+            pipe_xml.set('descritpion', pipe_description)            
+        
         # capture device properties
+        if classes_xml is not None and line.strip().startswith('@DeviceProperty'):
+
+            property_description = comment_buffer
+            comment_buffer = ''
+
+            # walk through other decorations
+            while source_lines[index].strip().startswith('@'):
+                index += 1
+
+            # property name and type
+            line = ' ' + source_lines[index].strip()
+            match_result = re.match(
+                r'.*(?=\s[a-zA-Z]+[[]]*\s+[a-zA-Z0-9]+\s*[=;])'
+                r'\s(?P<type>[a-zA-Z]+[[]]*)\s+(?P<name>[a-zA-Z0-9]+)\s*[=;]',
+                ' ' + line
+            )
+            if match_result is None:
+                continue
+            else:
+                property_name = match_result.group('name')
+                property_type = JAVA_DS_ATTRIBUTE_DATATYPES.get(
+                    match_result.group('type'),
+                    [match_result.group('type'), '']
+                )[0]
+
+            # property xml element
+            property_xml = etree.SubElement(classes_xml, 'deviceProperties')
+
+            # fill attributes
+            property_xml.set('name', property_name)
+            property_xml.set('description', property_description)
+
+            property_type_xml = etree.SubElement(property_xml, 'type')
+            property_type_xml.set(etree.QName('http://www.w3.org/2001/XMLSchema-instance', 'type'),
+                                  'pogoDsl:' + property_type)
 
         # capture class properties
+        if classes_xml is not None and line.strip().startswith('@ClassProperty'):
+
+            property_description = comment_buffer
+            comment_buffer = ''
+
+            # walk through other decorations
+            while source_lines[index].strip().startswith('@'):
+                index += 1
+
+            # property name and type
+            line = ' ' + source_lines[index].strip()
+            match_result = re.match(
+                r'.*(?=\s[a-zA-Z]+[[]]*\s+[a-zA-Z0-9]+\s*[=;])'
+                r'\s(?P<type>[a-zA-Z]+[[]]*)\s+(?P<name>[a-zA-Z0-9]+)\s*[=;]',
+                ' ' + line
+            )
+            if match_result is None:
+                continue
+            else:
+                property_name = match_result.group('name')
+                property_type = JAVA_DS_ATTRIBUTE_DATATYPES.get(
+                    match_result.group('type'),
+                    [match_result.group('type'), '']
+                )[0]
+
+            # property xml element
+            property_xml = etree.SubElement(classes_xml, 'deviceProperties')
+
+            # fill attributes
+            property_xml.set('name', property_name)
+            property_xml.set('description', property_description)
+
+            property_type_xml = etree.SubElement(property_xml, 'type')
+            property_type_xml.set(etree.QName('http://www.w3.org/2001/XMLSchema-instance', 'type'),
+                                  'pogoDsl:' + property_type)
 
         index += 1
-
-
-
-
-    
-
-    # generate commands xml    
-    for cmd_name, cmd in cmd_list.iteritems():
-        # xml element for the command
-        cmd_xml = etree.SubElement(classes_xml, 'commands')
-        
-        # populate fields
-        cmd_xml.set('name', cmd_name)
-        
-        if len(cmd) > 2:
-            cmd_xml.set('descritpion', cmd[2].get('description', ''))
-            
-            # if len(cmd[1].get('Polling period', '')) > 0:
-            #     cmd_xml.set('polledPerion', cmd[1].get('Polling period'))
-            # cmd_xml.set('descritpion', cmd[1].get('description', ''))
-        
-        # argin
-        argin_xml = etree.SubElement(cmd_xml, 'argin')        
-        argin_xml.set('description', cmd[0][1])
-        
-        argin_type_xml = etree.SubElement(argin_xml, 'type')
-        argin_type_xml.set(
-            etree.QName('http://www.w3.org/2001/XMLSchema-instance', 'type'), 
-            'pogoDsl:' + cmd[0][0]
-        )        
-            
-        # argout
-        argout_xml = etree.SubElement(cmd_xml, 'argout')
-        argout_xml.set('description', cmd[1][1])
-
-        argout_type_xml = etree.SubElement(argout_xml, 'type')
-        argout_type_xml.set(
-            etree.QName('http://www.w3.org/2001/XMLSchema-instance', 'type'),
-            'pogoDsl:' + cmd[1][0]
-        )
-
-    # generate class properties xml
-    for property_name, prop in class_property_list.iteritems():
-        # property xml element
-        property_xml = etree.SubElement(classes_xml, 'classProperties')
-
-        # fill attributes
-        property_xml.set('name', property_name)
-        property_xml.set('description', prop[1])
-
-        property_type_xml = etree.SubElement(property_xml, 'type')
-        property_type_xml.set(etree.QName('http://www.w3.org/2001/XMLSchema-instance', 'type'), 'pogoDsl:'+prop[0])
-
-    # generate device properties xml
-    for property_name, prop in device_property_list.iteritems():
-        # property xml element
-        property_xml = etree.SubElement(classes_xml, 'deviceProperties')
-
-        # fill attributes
-        property_xml.set('name', property_name)
-        property_xml.set('description', prop[1])
-
-        property_type_xml = etree.SubElement(property_xml, 'type')
-        property_type_xml.set(etree.QName('http://www.w3.org/2001/XMLSchema-instance', 'type'),
-                              'pogoDsl:' + prop[0])
 
     # return generated xml
     return '<?xml version="1.0" encoding="ASCII"?>' + etree.tostring(xmi_xml)
@@ -496,47 +380,5 @@ def get_xmi_from_java(name, family, java_file_url, element = None):
 
 if __name__ == "__main__":
     # main function perform tests
-
-    py_file1 = 'test-assets/BakeOutControlDS.py'
-
-    py_file2 = 'test-assets/LOCOSplitter.py'
-
-    with open(py_file1) as f:
-        source = f.read()
-
-        class_content = get_class_content(source.splitlines(), 'BakeOutControlDSClass')
-
-        print class_content
-
-        print 'Device properties:'
-        print get_class_dict_member(class_content, 'device_property_list')
-        print 'Class properties:'
-        print get_class_dict_member(class_content, 'class_property_list')
-        print 'Attributes:'
-        print get_class_dict_member(class_content, 'attr_list')
-        print 'Commands:'
-        print get_class_dict_member(class_content, 'cmd_list')
-        print
-
-    with open(py_file2) as f:
-        source = f.read()
-
-        class_content = get_class_content(source.splitlines(), 'LOCOSplitterClass')
-
-        print class_content
-
-        print 'Device properties:'
-        print get_class_dict_member(class_content, 'device_property_list')
-        print 'Class properties:'
-        print get_class_dict_member(class_content, 'class_property_list')
-        print 'Attributes:'
-        print get_class_dict_member(class_content, 'attr_list')
-        print 'Commands:'
-        print get_class_dict_member(class_content, 'cmd_list')
-        print
-
-    print
-    print get_xmi_from_python('BakeOutControlDS', 'Vacuum', 'file:' + py_file1)
-
-    print
-    print get_xmi_from_python('LOCOSplitter', 'RF', 'file:' + py_file2)
+    java_file = 'test-assets/ControlSystemMonitor.java'
+    print get_xmi_from_java('ControlSystemMonitor', 'SoftwareSystems', 'file:' + java_file)
