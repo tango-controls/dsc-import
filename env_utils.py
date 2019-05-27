e18a45acf970da9b99e7ab4854f1ffec3194ae93
Piotr Goryl <piotr.goryl@s2innovation.com>
2019-05-27 17:21:57 +0200

    progress #4 - basic list generation from ENV implemented

--- env_utils.py
+++ env_utils.py
@@ -17,10 +17,10 @@
 
 """
 
-import csv
+import os
 from pytz import utc
 from datetime import datetime
-import urllib2
+import urlparse
 
 
 def get_device_servers_list():
@@ -31,17 +31,19 @@ def get_device_servers_list():
 
         Environment variables in use:
 
-        * `DSC_XMI_URL` or `DSC_PYTHON_URL` or `DSC_PYTHON_HL_URL` or `DSC_JAVA_URL` defines where to find information
+        * `DSC_XMI_URL` or `DSC_PYTHON_URL` or `DSC_JAVA_URL` defines where to find information
           to get or build and .xmi file. One of these shall be provided,
+        * `DSC_DEVICE_SERVER_NAME` - to specify device server name,
         * `DSC_REPOSITORY_URL` to specify URL of the repository,
         * `DSC_REPOSITORY_TYPE` should be one of: `GIT`, `SVN`, `Mercurial`, `FTP`, `Other`,
         * Optional `DSC_RELEASE_TAG` to specify device server release,
         * Optional `DSC_README_URL` - to specify where to find a README file for this device class,
-        * Optional `DSC_DOC_URL` - to specify additional documentation for this DS. Format is: *title, URL*
-          (like `"Pogo genereated manual, https://someserver.org/ds_doc"`),
-        * Optional `DSC_DEVICE_SERVER_NAME` - to specify device server name if it is different than the device class,
-        * Optional `DSC_DEVICE_SERVER__DESCRIPTION` - to specify/override device server description,
-        * Optional `DSC_CONTACT` - to provide or override contact info from .xmi file.
+        * Optional `DSC_DOC_URL` - to specify additional documentation for this DS,
+        * Optional `DSC_DOC_TITLE` - to specify title of additional documentation,
+        * Optional `DSC_DOC_TYPE` - to specify title of additional documentation,
+        * Optional `DSC_DEVICE_SERVER_DESCRIPTION` - to specify/override device server description,
+        * Optional `DSC_CONTACT` - to provide or override contact info from .xmi file,
+        * Optional `DSC_FAMILY` - to provide or override family info from .xmi file.
 
 
     """
@@ -49,105 +51,62 @@ def get_device_servers_list():
     # this is the list to be returned
     ds_list = []
 
-    # open the file
-    with open(csv_file_path, 'rb') as csv_file:
+    # this is a dict of a device server
+    ds = {
+        'name': os.environ.get('DSC_DEVICE_SERVER_NAME'),
+        'xmi_files': [],
+        'py_files': [],
+        'java_files': [],
+        'readme_files': [],
+        'repository_url': os.environ.get('DSC_REPOSITORY_URL'),
+        'repository_type': os.environ.get('DSC_REPOSITORY_TYPE'),
+        'tag': os.environ.get('DSC_RELEASE_TAG', os.environ.get('DSC_REPOSITORY_TAG', '')),
+    }
 
-        # treat the file as dictionary
-        rows = csv.DictReader(csv_file)
-        row_index = 0
+    # get the URL
+    url = ''
+    file_key = None
 
-        # iterate through rows
-        for row in rows:
-            # just for counting:
-            row_index += 1
+    if os.environ.get('DSC_XMI_URL') not in [None, '']:
+        url = os.environ.get('DSC_XMI_URL')
+        file_key = 'xmi'
 
-            try:
+    elif os.environ.get('DSC_PYTHON_URL') not in [None, '']:
+        url = os.environ.get('DSC_PYTHON_URL')
+        file_key = 'py'
 
-                ds = dict()
+    elif os.environ.get('DSC_JAVA_URL') not in [None, '']:
+        url = os.environ.get('DSC_JAVA_URL')
+        file_key = 'java'
 
-                ds['name'] = row.get('name', '')
+    # retrieve information from the URL
+    url_parts = urlparse.urlsplit(url)
 
-                # process repository info
-                ds['repository_url'] = row.get('repository_url', None)
-                if ds['repository_url'] is None:
-                    # device server shell be available in some repository
-                    print 'Device server %s is not in any repository.' % ds['name']
-                    continue
+    ds['{0}_files'.format(file_key)] = {
+        'name': url_parts.path.split('/')[-1],
+        '{0}_url'.format(file_key): url,
+        'element': {'date': datetime.now(utc)},
+    }
 
-                ds['repository_type'] = row.get('repository_type', '')
-                if ds['repository_type'].strip() not in ['GIT', 'SVN', 'Mercurial', 'FTP', 'Other']:
-                    if 'git' in ds['repository_url'].lower():
-                        ds['repository_type'] = 'GIT'
-                    elif 'svn' in ds['repository_url'].lower():
-                        ds['repository_type'] = 'SVN'
-                    else:
-                        ds['repository_type'] = 'Other'
+    # add a readme if available
+    if os.environ.get('DSC_README_URL') not in [None, '']:
+        url_parts = urlparse.urlsplit('DSC_README_URL')
+        ds['readme_files'].append({
+            'name': url_parts.path.split('/')[-1],
+            'path': '',
+            'readme_url': os.environ.get('DSC_README_URL'),
+            'element': {'date': datetime.now(utc)},
+        })
 
-                if len(row.get('tag', '')) > 0:
-                    ds['tag'] = row.get('tag', '')
+    # process other documentation related columns
+    if os.environ.get('DSC_DOC_URL') not in [None, '']:
+        ds['documentation_url'] = os.environ.get('DSC_DOC_URL')
+        ds['documentation_title'] = os.environ.get('DSC_DOC_TITLE', 'Generated by Pogo')
+        ds['documentation_type'] = os.environ.get('DSC_DOC_TYPE', 'Generated')
 
-                # process .xmi related columns
-                xmi_urls_list = []
-                xmi_files = []
-                if row.get('xmi_files_urls', '').startswith('['):
-                    xmi_urls_list = eval(row.get('xmi_files_urls'))
-                elif len(row.get('xmi_files_urls', '')) > 0:
-                    xmi_urls_list.append(row.get('xmi_files_urls'))
+    # TODO: implement support for meta-data (family, contact, ..)
 
-                index = 0
-                for xmi_url in xmi_urls_list:
 
-                    xmi = {
-                        'name': ds['name']+str(index)+'.xmi',
-                        'path': '',
-                        'element': {'date': datetime.now(utc)},
-                        'xmi_url': xmi_url,
-                    }
-
-                    if row.get('upload_xmi_file', '') in [1, '1', 'true', 'True']:
-                        # download content of the xmi file then upload it to the catalogue
-                        xmi['content'] = urllib2.urlopen(xmi_url).read()
-
-                    xmi_files.append(xmi)
-
-                    index += 1
-
-                ds['xmi_files'] = xmi_files
-
-                # for device servers featuring pogo documentation but no .xmi:
-                if len(row.get("pogo_docs_url_base", '')) > 0:
-                    ds['pogo_docs_url_base'] = row.get("pogo_docs_url_base")
-                    ds['pogo_description_html'] = row.get('pogo_description_html', 'ClassDescription.html')
-                    ds['pogo_attributes_html'] = row.get('pogo_attributes_html', 'Attributes.html')
-                    ds['pogo_commands_html'] = row.get('pogo_commands_html', 'Commands.html')
-                    ds['pogo_properties_html'] = row.get('pogo_properties_html', 'Properties.html')
-
-                # process readme related columns
-                ds['readme_files'] = []
-
-                if len(row.get('readme_url', '')) > 0:
-
-                    ds['readme_files'].append({
-                        'name': row.get('readme_name', 'README.txt'),
-                        'path': '',
-                        'readme_url': row.get('readme_url'),
-                        'element': {'date': datetime.now(utc)},
-                    })
-
-                # process other documentation related columns
-                if len(row.get('documentation_url', '')) > 0:
-                    ds['documentation_url'] = row.get('documentation_url')
-                    ds['documentation_title'] = row.get('documentation_title', 'Generated by Pogo.')
-                    ds['documentation_type'] = row.get('documentation_type', 'Generated')
-
-                # append device servers list
-                ds_list.append(ds)
-
-            except Exception as e:
-                print
-                print 'Error in processing row %d: ' % row_index
-                print e.message
-                print
-                print 'This device server will not be included in an update list.'
+    ds_list.append(ds)
 
     return ds_list
